67e19db1e2990904efa8f54f688e8c560a588855
msaqrinktank <47291189+msaqrinktank@users.noreply.github.com>
2021-04-07 01:37:25 +0200

    create todo app

---
+++ todo.js
@@ -0,0 +1,17 @@
+let toDo = {
+    toDoObject(id, title, content) {
+        this.id = id
+        this.title = title
+        this.content = content
+    },
+    toDoList: [],
+    addTodo() {
+
+    },
+    editTodo() {
+
+    },
+    deleteTodo() {
+
+    }
+}
